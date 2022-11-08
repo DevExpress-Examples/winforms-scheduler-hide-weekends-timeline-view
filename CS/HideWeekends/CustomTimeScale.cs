96f2be5afc0366b0f9ae449a654480202f5b0226
NadezhdaDX <37904867+NadezhdaBobrovaDX@users.noreply.github.com>
2022-11-08 17:53:34 +0400

    Update CustomTimeScale.cs

--- CS/HideWeekends/CustomTimeScale.cs
+++ CS/HideWeekends/CustomTimeScale.cs
@@ -20,35 +20,13 @@ using DevExpress.XtraScheduler.Native;
 
 namespace TimelineTimeScales {
 
-    public class TimeScaleWorkWeekDay : TimeScale {
+    public class TimeScaleWorkWeekDay : TimeScaleDay {
         protected override string DefaultDisplayFormat { get { return "d ddd"; } }
         protected override string DefaultMenuCaption { get { return "WorkWeek"; } }
 
-        protected override TimeSpan SortingWeight {
-            get { return TimeSpan.FromDays(1.1); }
-        }
-        public override DateTime Floor(DateTime date) {
-            DateTime start = date.Date;
-            if(start.DayOfWeek == DayOfWeek.Saturday)
-                return start.AddDays(-1);
-            if(start.DayOfWeek == DayOfWeek.Sunday)
-                return start.AddDays(-2);
-            return start;
-        }
-        protected override bool HasNextDate(DateTime date) {
-            int days = GetNextDayOffset(date.DayOfWeek);
-            return date <= DateTime.MaxValue.AddDays(-days);
-        }
-        public override DateTime GetNextDate(DateTime date) {
-            int days = GetNextDayOffset(date.DayOfWeek);
-            return date.AddDays(days);
-        }
-        protected int GetNextDayOffset(DayOfWeek dayOfWeek) {
-            if(dayOfWeek == DayOfWeek.Friday)
-                return 3;
-            if(dayOfWeek == DayOfWeek.Saturday)
-                return 2;
-            return 1;
+        public override bool IsDateVisible(DateTime date)
+        {
+            return date.DayOfWeek != DayOfWeek.Sunday && date.DayOfWeek != DayOfWeek.Saturday;
         }
     }
 }
