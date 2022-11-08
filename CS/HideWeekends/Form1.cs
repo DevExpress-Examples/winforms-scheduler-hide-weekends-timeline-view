b8029f412dd9ad6e5b7227f482e277ff0e35b8fb
NadezhdaDX <37904867+NadezhdaBobrovaDX@users.noreply.github.com>
2022-11-08 17:54:27 +0400

    Update Form1.cs

--- CS/HideWeekends/Form1.cs
+++ CS/HideWeekends/Form1.cs
@@ -23,7 +23,7 @@ namespace TimelineTimeScales {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
-            schedulerControl1.OptionsView.FirstDayOfWeek = FirstDayOfWeek.Monday;
+         schedulerControl1.OptionsView.FirstDayOfWeek = FirstDayOfWeek.Monday;
             HideWeekends(false);
 
         }
@@ -52,9 +52,6 @@ namespace TimelineTimeScales {
                 finally {
                     scales.EndUpdate();
                 }
-
-                checkEdit2.Enabled = true;
-
             }
             else {
                 scales.BeginUpdate();
@@ -68,54 +65,8 @@ namespace TimelineTimeScales {
                 finally {
                     scales.EndUpdate();
                 }
-
-                checkEdit2.Enabled = false;
             }
         
         }
-
-        private void schedulerControl1_CustomDrawTimeCell(object sender, CustomDrawObjectEventArgs e) {
-            DevExpress.XtraScheduler.SchedulerControl control = (DevExpress.XtraScheduler.SchedulerControl)sender;
-            if(control.ActiveViewType != DevExpress.XtraScheduler.SchedulerViewType.Timeline)
-                return;
-
-            DevExpress.XtraScheduler.Drawing.TimeCell cell = (DevExpress.XtraScheduler.Drawing.TimeCell)e.ObjectInfo;
-            if(cell.Selected && (control.Focused || !control.OptionsView.HideSelection)) {
-                cell.SelectionAppearance.FillRectangle(cell.Cache, cell.ContentBounds);
-                e.Handled = true;
-                return;
-            }
-
-            bool containsWeekDays = schedulerControl1.WorkDays.IsWorkDay(cell.Interval.Start);
-
-            Color color = containsWeekDays ? this.schedulerControl1.ResourceColorSchemas[0].CellLight : this.schedulerControl1.ResourceColorSchemas[0].Cell;
-            cell.Cache.FillRectangle(cell.Cache.GetSolidBrush(color), cell.ContentBounds);
-            Color borderColor;
-            if(containsWeekDays)
-                borderColor = cell.EndOfHour ? this.schedulerControl1.ResourceColorSchemas[0].CellBorderDark : this.schedulerControl1.ResourceColorSchemas[0].CellBorder;
-            else
-                borderColor = cell.EndOfHour ? this.schedulerControl1.ResourceColorSchemas[0].CellLightBorderDark : this.schedulerControl1.ResourceColorSchemas[0].CellLightBorder;
-            cell.Cache.FillRectangle(cell.Cache.GetSolidBrush(borderColor), cell.BottomBorderBounds);
-            cell.Cache.FillRectangle(cell.Cache.GetSolidBrush(borderColor), cell.LeftBorderBounds);
-            cell.Cache.FillRectangle(cell.Cache.GetSolidBrush(borderColor), cell.RightBorderBounds);
-
-            e.Handled = true;
-        }
-
-        private void checkEdit2_CheckedChanged(object sender, EventArgs e) {
-            CheckEdit editor = sender as CheckEdit;
-            if(editor.Checked) {
-                schedulerControl1.CustomDrawTimeCell += schedulerControl1_CustomDrawTimeCell;
-                schedulerControl1.Refresh();
-            }
-            else {
-                schedulerControl1.CustomDrawTimeCell -= schedulerControl1_CustomDrawTimeCell;
-                schedulerControl1.Refresh();
-            }
-
-
-        }
-
-
     }
 }
